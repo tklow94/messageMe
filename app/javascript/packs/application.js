a8cfab2da6dbc7c806f8adeed24ceb61487d93ea
Trevor <tlow94@uw.edu>
2021-01-06 12:33:52 -0800

    login message controller restrictions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 require("semantic-ui-sass")
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on('turbolinks:load', function(){$(".ui.dropdown").dropdown();     $('.message .close').on('click', function(){ $(this).closest('.message').transition('fade')     }); });
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
