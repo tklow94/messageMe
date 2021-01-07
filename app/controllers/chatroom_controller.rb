3edcabb1555e1bf6c6c960c254efb156461c6ba4
Trevor <tlow94@uw.edu>
2021-01-06 16:50:42 -0800

    styling

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,7 +3,9 @@ class ChatroomController < ApplicationController
 
     def index
         @message = Message.new
-        @messages = Message.all
+        @messages = Message.custom_display
     end
+
+  
     
 end
